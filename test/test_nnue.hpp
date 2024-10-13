e62aaa0d1a59c79b03008562c3e871cc8fa80b67
octopus-prime <mike.gresens@gmail.com>
2024-10-13 14:20:39 +0200

    Evaluation with update

--- test/test_nnue.hpp
+++ test/test_nnue.hpp
@@ -26,33 +26,68 @@ void test_nnue() {
     if (!description.ends_with("with the https://github.com/official-stockfish/nnue-pytorch trainer."))
         throw std::runtime_error{"test_nnue (description) failed"};
 
-    const std::uint16_t white_features[32] = {
-        make_index<WHITE>(SQ_A1, SQ_A1, W_KING),
-        make_index<WHITE>(SQ_A1, SQ_C3, W_PAWN),
-        make_index<WHITE>(SQ_A1, SQ_B8, B_KING),
-        make_index<WHITE>(SQ_A1, SQ_D4, B_ROOK)
+    NNUE::Accumulator accumulator[2];
+    std::int32_t score[2];
+
+    // evaluation with refresh
+
+    const std::uint16_t active_features[2][32] = { {
+            make_index<WHITE>(SQ_A1, SQ_A1, W_KING),
+            make_index<WHITE>(SQ_A1, SQ_B8, B_KING),
+            make_index<WHITE>(SQ_A1, SQ_C3, W_PAWN),
+            make_index<WHITE>(SQ_A1, SQ_D4, B_ROOK)
+        }, {
+            make_index<BLACK>(SQ_B8, SQ_A1, W_KING),
+            make_index<BLACK>(SQ_B8, SQ_B8, B_KING),
+            make_index<BLACK>(SQ_B8, SQ_C3, W_PAWN),
+            make_index<BLACK>(SQ_B8, SQ_D4, B_ROOK)
+        }
     };
-    const std::uint16_t black_features[32] = {
-        make_index<BLACK>(SQ_B8, SQ_B8, B_KING),
-        make_index<BLACK>(SQ_B8, SQ_D4, B_ROOK),
-        make_index<BLACK>(SQ_B8, SQ_A1, W_KING),
-        make_index<BLACK>(SQ_B8, SQ_C3, W_PAWN)
-    };
 
-    NNUE::Accumulator accumulator;
-    nnue.refresh<WHITE>(accumulator, std::span{white_features}.first(4));
-    nnue.refresh<BLACK>(accumulator, std::span{black_features}.first(4));
+    nnue.refresh<WHITE>(accumulator[0], std::span{active_features[WHITE]}.first(4));
+    nnue.refresh<BLACK>(accumulator[0], std::span{active_features[BLACK]}.first(4));
 
-    const std::int32_t white_score = nnue.evaluate<WHITE>(accumulator, 4);
-    const std::int32_t black_score = nnue.evaluate<BLACK>(accumulator, 4);
+    score[WHITE] = nnue.evaluate<WHITE>(accumulator[0], 4);
+    score[BLACK] = nnue.evaluate<BLACK>(accumulator[0], 4);
 
-    std::printf("%d\n", white_score);
-    std::printf("%d\n", black_score);
+    std::printf("%d\n", score[WHITE]);
+    std::printf("%d\n", score[BLACK]);
 
-    if (white_score != -887)
+    if (score[WHITE] != -887)
         throw std::runtime_error{"test_nnue (white) failed"};
 
-    if (black_score != 1317)
+    if (score[BLACK] != 1317)
+        throw std::runtime_error{"test_nnue (black) failed"};
+
+    // evaluation with update
+
+    const std::uint16_t removed_features[2][3] = { {
+            make_index<WHITE>(SQ_A1, SQ_C3, W_PAWN),
+            make_index<WHITE>(SQ_A1, SQ_D4, B_ROOK)
+        }, {
+            make_index<BLACK>(SQ_B8, SQ_C3, W_PAWN),
+            make_index<BLACK>(SQ_B8, SQ_D4, B_ROOK)
+    }};
+
+    const std::uint16_t added_features[2][3] = { {
+            make_index<WHITE>(SQ_A1, SQ_D4, W_PAWN)
+        }, {
+            make_index<BLACK>(SQ_B8, SQ_D4, W_PAWN)
+    }};
+
+    nnue.update<WHITE>(accumulator[1], accumulator[0], std::span{removed_features[WHITE]}.first(2), std::span{added_features[WHITE]}.first(1));
+    nnue.update<BLACK>(accumulator[1], accumulator[0], std::span{removed_features[BLACK]}.first(2), std::span{added_features[BLACK]}.first(1));
+
+    score[WHITE] = nnue.evaluate<WHITE>(accumulator[1], 3);
+    score[BLACK] = nnue.evaluate<BLACK>(accumulator[1], 3);
+
+    std::printf("%d\n", score[WHITE]);
+    std::printf("%d\n", score[BLACK]);
+
+    if (score[WHITE] != 135)
+        throw std::runtime_error{"test_nnue (white) failed"};
+
+    if (score[BLACK] != -113)
         throw std::runtime_error{"test_nnue (black) failed"};
 }
 
